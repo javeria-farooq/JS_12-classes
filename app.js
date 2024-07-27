03b7ff0e2863d175980963e6ccf9fce99de77bcf
javeria-farooq <gettingeducation6@gmail.com>
2024-07-27 16:58:54 +0500

    Maintain code

--- app.js
+++ app.js
@@ -1,128 +1,192 @@
-/*class toyotaCar {
-    fullName = 'javeria'
-    age = 20
-    
-    start(){
-        console.log("start")
-    }
-
-    stop() {
-        console.log("stop")
-    }
-
-    setBrand() {
-        this.brandName = "fortuner"
-    }
-}
-
-let fortuner = new toyotaCar()
-console.log(fortuner)
-
-fortuner.setBrand()
-console.log(fortuner.setBrand)
-fortuner.start()
-
-console.log(fortuner)*/
-
-/*constructor */
-
-/*console.log("constructor:")
-
-class toyotaCar2 {
+/*// car template
+class Car {
     constructor(brand, mileAge) {
         this.brand = brand
         this.mileAge = mileAge
     }
-}
 
-console.log(toyotaCar2)
-console.log(constructor)
+    start() {
+        return(`${this.brand} is starting...`)
+    }
 
-let fortuner2 = new toyotaCar2("fortuner", 10)
-console.log(fortuner2)
-console.log("constructor = ",fortuner2.constructor)
-let lexus2 = new toyotaCar2()
-console.log(lexus2)
-lexus2.brand = 'javeria'*/
-
-/*extends: */
-
-/*class parent {
-    hello() {
-        console.log("hello")
+    stop(){
+        return(`${this.brand} has stopped.`)
     }
 }
 
-class child extends parent{}
+let fortuner = new Car('fortuner', 8)
+console.log(fortuner)
+console.log(fortuner.start())
+console.log(fortuner.stop())
 
-let obj = new child()
-console.log(obj)*/
+let lexus = new Car('lexus', 10)
+console.log(lexus)
+console.log(lexus.start())
+console.log(lexus.stop())
 
-/*class person {
+let toyota = new Car('toyota', 15)
+console.log(toyota)
+console.log(toyota.start())
+console.log(toyota.stop())*/
+
+/*//parent template
+class parent {
+    constructor(name, age) {
+        this.name = name
+        this.age = age
+    }
+
+    work() {
+        console.log('parents working...')
+    }
+}
+
+class mother extends parent{
+    constructor(name, age) {
+        super(name, age)
+        this.gender = 'female'
+    }
+    cook() {
+        console.log('mother cooks food.')
+    }
+    dusting() {
+        console.log('dusting...')
+    }
+}
+
+let motherObj = new mother('yasmeen', 42)
+console.log(motherObj)
+motherObj.work()
+motherObj.cook()
+motherObj.dusting()
+
+class father extends parent{
+    constructor(name, age) {
+        super(name, age)
+        this.gender = 'male'
+    }
+
+    job() {
+        console.log('father is going to a job')
+    }
+}
+
+let fatherObj = new father('farooq', 50)
+console.log(fatherObj)
+fatherObj.work()
+fatherObj.job()*/
+
+/*//person template
+class Person {
     constructor(name) {
         this.name = name
-        this.species  = "homo sapiens"
+        this.species = 'homo sapiens'
     }
+
     eat() {
-        console.log("eat")
+        return(`${this.name} can eat.`)
     }
 
     sleep() {
-        console.log("sleep")
+        return(`${this.name} can sleep.`)
+    }
+
+    walk() {
+        return(`${this.name} can walk.`)
     }
 }
 
-class engineer extends person {
-    constructor(name) {
-        super(name)
-    }
-    work() {
-        super.eat()
-        console.log("solve problems, build something...")
-    }
-}
-
-let personObj = new person("javeria")
-console.log(`person object = `)
+let personObj = new Person('javeria')
 console.log(personObj)
+console.log(personObj.eat())
+console.log(personObj.sleep())
+console.log(personObj.walk())
 
-let engObj = new engineer("javeria")
-console.log(`Engineer object = `)
-console.log(engObj)*/
+class Engineer extends Person{
+    constructor(name, department) {
+        super(name)
+        this.department = department
+    }
 
-/*practice question */
+    work() {
+        return `${this.name} solving problems and building something new.`
+    }
+}
 
-/*let data = 'secret information';
+let engineer = new Engineer('daniyal nagori', 'software engineer')
+console.log(engineer)
+console.log(engineer.eat())
+console.log(engineer.sleep())
+console.log(engineer.walk())
+console.log(engineer.work())
 
-class user {
+class Doctor extends Person{
+    constructor(name, spiciality) {
+        super(name)
+        this.spiciality = spiciality
+    }
+
+    work() {
+        return `${this.name} is curing patients.`
+    }
+}
+
+let doctor = new Doctor('ahmed', 'skin specialist')
+console.log(doctor)
+console.log(doctor.eat())
+console.log(doctor.sleep())
+console.log(doctor.walk())
+console.log(doctor.work())*/
+
+//practice question
+let data = 'secret information'
+
+class User {
     constructor(name, email) {
         this.name = name
         this.email = email
     }
 
     viewData() {
-        console.log(`data = ${data}`)
+        console.log(`${this.name}! data = ${data}.`)
     }
 }
 
-let stu1 = new user('javeria', 'javeria@gmail.com')
-let stu2 = new user('afnan', 'afnan@gmail.com')
-let stu3 = new user('aliza', 'aliza@gmail.com')
-console.log(stu1)
-console.log(stu2)
-console.log(stu3)
+let user1 = new User('javeria', 'javeria@gmail.com')
+console.log(user1)
+user1.viewData()
 
-class admin extends user {
+let user2 = new User('aliza', 'aliza@gmail.com')
+console.log(user2)
+user2.viewData()
+
+let user3 = new User('afnan', 'afnan@gmail.com')
+console.log(user3)
+user3.viewData()
+
+class Admin extends User{
     constructor(name, email) {
         super(name, email)
     }
+
     editData() {
+        console.log(`${this.name}! You can edit your data.`)
         data = 'edit college data'
-       console.log(`data = ${data}`)       
+        console.log(`${this.name}! Now the data is : ${data}`)
     }
 }
 
-let teach1 = new admin('admin 1', 'admin1@clg.com')
-let teach2 = new admin('admin 2', 'admin2@clg.com')
-console.log(teach1)
-console.log(teach2)*/
+let admin1 = new Admin('admin 1', 'admin1@gmail.com')
+console.log(admin1)
+admin1.viewData()
+admin1.editData()
+
+let admin2 = new Admin('admin 2', 'admin2@gmail.com')
+console.log(admin2)
+admin2.viewData()
+admin2.editData()
+
+let admin3 = new Admin('admin 3', 'admin3@gmail.com')
+console.log(admin3)
+admin3.viewData()
+admin3.editData()
